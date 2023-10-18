6398e9beac8622920a9338bb2634392693687154
afeezdev <afeezdev@gmail.com>
2023-10-18 21:50:12 +0100

    password checker

---
+++ checkmypass.py
@@ -0,0 +1,14 @@
+import requests
+
+def request_api_data(query_char):
+    url = 'https://api.pwnedpasswords.com/range/' + query_char
+    res = requests.get(url)
+    if res.status_code != 200:
+        raise RuntimeError(f'Error fetching: {res.status_code}, check the api and try again')
+    return res
+
+def pwned_api_check(password):
+    # check if password exist in API response
+    pass
+
+request_api_data('123')
