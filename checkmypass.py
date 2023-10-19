c647c4ea9516ae5a81037b48938a6e668cd973a4
afeezdev <afeezdev@gmail.com>
2023-10-19 21:24:09 +0100

    password checking implemented

--- checkmypass.py
+++ checkmypass.py
@@ -1,4 +1,6 @@
 import requests
+import hashlib
+import sys
 
 def request_api_data(query_char):
     url = 'https://api.pwnedpasswords.com/range/' + query_char
@@ -7,8 +9,26 @@ def request_api_data(query_char):
         raise RuntimeError(f'Error fetching: {res.status_code}, check the api and try again')
     return res
 
-def pwned_api_check(password):
-    # check if password exist in API response
-    pass
+def get_password_leaks_count(hashes, hash_to_check):
+    hashes = (line.split(':') for line in hashes.text.splitlines())
+    for h, count in hashes:
+        if h == hash_to_check:
+            return count
+    return 0
 
-request_api_data('123')
+def pwned_api_check(password):
+    sha1password = hashlib.sha1(password.encode('utf-8')).hexdigest().upper()
+    first5_char, tail = sha1password[:5], sha1password[5:]
+    response = request_api_data(first5_char)  
+    return get_password_leaks_count(response, tail )
+    
+def main(args):
+    for password in args:
+        count = pwned_api_check(password)
+        if count:
+            print(f'{password} was found {count} times....you should probably change your password')
+        else:
+            print(f'{password} was NOT found. carry on!')
+    return 'done'
+
+main(sys.argv[1:])
